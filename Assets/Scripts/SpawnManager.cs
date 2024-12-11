b4ab3c4bc722c07f7640f1833f6bfb034ba7e29c
NgoVanLoc <62205ngovanloc@gmail.com>
2024-12-11 17:53:24 +0700

    Complete

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+
+    public GameObject[] animalPrefabs;
+
+    private float spanwnRangeX = 20;
+    private float spanwnPosY = 20;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f; 
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    void SpawnRandomAnimal()
+    {
+        // Sinh ngẫu nhiên vị trí
+        Vector3 spanwPos = new Vector3(Random.Range(-spanwnRangeX, spanwnRangeX), 0, spanwnPosY);
+
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Instantiate(animalPrefabs[animalIndex], spanwPos, animalPrefabs[animalIndex].transform.rotation);
+    }
+}
